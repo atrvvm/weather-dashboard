870202a2b47f9390a6bc6c7d6dbe47c0be4fef20
Anna T. <annangoctram1@gmail.com>
2025-01-03 01:36:18 -0500

    (feat) Add code to route server and html files

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -4,6 +4,7 @@ const router = Router();
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
+
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
