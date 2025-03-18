ba3bf37965bf5d7defce4c5c9535f9c39a938be1
unknown <annangoctram1@gmail.com>
2025-03-18 03:24:09 -0400

    Update project structure and files to starter code

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -1,14 +1,10 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { Router , Response} from 'express';
-
+import { Router } from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// ✅ Define route to serve index.html
-router.get('*', (_, res: Response) => {
-    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
-  });
-  
+// TODO: Define route to serve index.html
+
 export default router;
